99d41f31968fc4a0728d7863cf7190a1a88a7963
bruninjaman <brubaco@hotmail.com>
2015-12-06 13:43:23 -0200

    Be Aware Ninja
    - Fixed some variables for more performance. (thanks jumpering)

--- Be Aware Ninja/Be Aware Ninja/Be Aware Ninja.cs
+++ Be Aware Ninja/Be Aware Ninja/Be Aware Ninja.cs
@@ -30,69 +30,70 @@ namespace Be_Aware_Ninja
             foreach (var v in enemies)
             {
                 string index;
-                if (v.Modifiers.Any(x => x.Name == "modifier_mirana_moonlight_shadow") && Utils.SleepCheck("mirana")) //mirana
+                System.Collections.Generic.IEnumerable<Modifier> mod = v.Modifiers.ToList(); // Ty Jumpering
+                if (mod.Any(x => x.Name == "modifier_mirana_moonlight_shadow") && Utils.SleepCheck("mirana")) //mirana
                 {
                     MessageCreator("mirana", "mirana_invis");
                     Utils.Sleep(15000,"mirana");
                 }
-                if (v.Modifiers.Any(x => x.Name == "modifier_alchemist_unstable_concoction") && Utils.SleepCheck("alch")) //alch
+                if (mod.Any(x => x.Name == "modifier_alchemist_unstable_concoction") && Utils.SleepCheck("alch")) //alch
                 {
                     MessageCreator("alchemist", "alchemist_unstable_concoction");
                     Utils.Sleep(15000, "alch");
                 }
-                if (v.Modifiers.Any(x => x.Name == "modifier_morphling_replicate_timer") && Utils.SleepCheck("morph")) //morph
+                if (mod.Any(x => x.Name == "modifier_morphling_replicate_timer") && Utils.SleepCheck("morph")) //morph
                 {
                     MessageCreator("morphling", "morphling_replicate");
                     Utils.Sleep(15000, "morph");
                 }
-                if (v.Modifiers.Any(x => x.Name == "modifier_ember_spirit_fire_remnant_timer") && Utils.SleepCheck("mirana")) // ember
+                if (mod.Any(x => x.Name == "modifier_ember_spirit_fire_remnant_timer") && Utils.SleepCheck("mirana")) // ember
                 {
                     MessageCreator("ember_spirit", "ember_spirit_fire_remnant");
                     Utils.Sleep(15000, "ember");
                 }
-                if (v.Modifiers.Any(x => x.Name == "modifier_bloodseeker_thirst_speed") && Utils.SleepCheck("bloodseeker")) //bloodseeker
+                if (mod.Any(x => x.Name == "modifier_bloodseeker_thirst_speed") && Utils.SleepCheck("bloodseeker")) //bloodseeker
                 {
                     MessageCreator("bloodseeker", "bloodseeker_thirst");
                     Utils.Sleep(15000, "bloodseeker");
                 }
-                if (v.Modifiers.Any(x => x.Name == "modifier_invoker_ghost_walk_enemy") && Utils.SleepCheck("invoker")) //invoker
+                if (mod.Any(x => x.Name == "modifier_invoker_ghost_walk_enemy") && Utils.SleepCheck("invoker")) //invoker
                 {
                     MessageCreator("invoker", "invoker_ghost_walk");
                     Utils.Sleep(15000, "invoker");
                 }
-                if (v.Modifiers.Any(x => x.Name == "modifier_item_invisibility_edge_windwalk") && Utils.SleepCheck("shadowblade")) //shadow blade
+                if (mod.Any(x => x.Name == "modifier_item_invisibility_edge_windwalk") && Utils.SleepCheck("shadowblade")) //shadow blade
                 {
                     index = v.Name.Remove(0, 14);
                     MessageItemCreator(index, "invis_sword");
                     Utils.Sleep(15000, "shadowblade");
                 }
-                if (v.Modifiers.Any(x => x.Name == "modifier_item_silver_edge_windwalk") && Utils.SleepCheck("silveredge")) // silver edge
+                if (mod.Any(x => x.Name == "modifier_item_silver_edge_windwalk") && Utils.SleepCheck("silveredge")) // silver edge
                 {
                     index = v.Name.Remove(0, 14);
                     MessageItemCreator(index, "silver_edge");
                     Utils.Sleep(15000, "silveredge");
                 }
-                if (v.Modifiers.Any(x => x.Name == "modifier_clinkz_wind_walk") && Utils.SleepCheck("clinkz")) // Clinkz
+                if (mod.Any(x => x.Name == "modifier_clinkz_wind_walk") && Utils.SleepCheck("clinkz")) // Clinkz
                 {
                     MessageCreator("clinkz", "clinkz_wind_walk");
                     Utils.Sleep(15000, "clinkz");
                 }
-                if (v.Modifiers.Any(x => x.Name == "modifier_teleporting") && v.ClassID == ClassID.CDOTA_Unit_Hero_Wisp && Utils.SleepCheck("wisp")) // IO
+                if (mod.Any(x => x.Name == "modifier_teleporting") && v.ClassID == ClassID.CDOTA_Unit_Hero_Wisp && Utils.SleepCheck("wisp")) // IO
                 {
                     MessageCreator("wisp", "wisp_relocate");
                     Utils.Sleep(15000, "wisp");
                 }
-                if (v.Modifiers.Any(x => x.Name == "modifier_bounty_hunter_wind_walk") && Utils.SleepCheck("bounty")) // BOUNTY
+                if (mod.Any(x => x.Name == "modifier_bounty_hunter_wind_walk") && Utils.SleepCheck("bounty")) // BOUNTY
                 {
                     MessageCreator("bounty_hunter", "bounty_hunter_wind_walk");
                     Utils.Sleep(15000, "bounty");
                 }
-                if (v.Modifiers.Any(x => x.Name == "modifier_nyx_assassin_vendetta") && Utils.SleepCheck("nyx")) // NYX ASSASSIN
+                if (mod.Any(x => x.Name == "modifier_nyx_assassin_vendetta") && Utils.SleepCheck("nyx")) // NYX ASSASSIN
                 {
                     MessageCreator("nyx_assassin", "nyx_assassin_vendetta");
                     Utils.Sleep(15000, "nyx");
                 }
-                if (v.Modifiers.Any(x => x.Name == "modifier_teleporting") && v.ClassID == ClassID.CDOTA_Unit_Hero_Furion && Utils.SleepCheck("furion")) // furion
+                if (mod.Any(x => x.Name == "modifier_teleporting") && v.ClassID == ClassID.CDOTA_Unit_Hero_Furion && Utils.SleepCheck("furion")) // furion
                 {
                     MessageCreator("furion", "furion_teleportation");
                     Utils.Sleep(15000, "furion");
