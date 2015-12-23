fde98bd3de06844a9aa96708fc9b85b71c806917
bruninjaman <brubaco@hotmail.com>
2015-12-23 06:23:25 -0200

    Auto Urn 1.2 Update
    - Fixed Menu
    - Auto bottle for friends

--- AutoItems/AutoItems/AutoItems_NINJA.cs
+++ AutoItems/AutoItems/AutoItems_NINJA.cs
@@ -12,6 +12,7 @@ namespace AutoItems
     {
         private static Hero me;
         private static Item item_bottle, item_phase_boots, item_magic_stick, item_magic_wand;
+        private static List<Hero> Alies;
         private static double PercentStickUse = 0.2;
         private static readonly uint WM_MOUSEWHEEL = 0x020A;
         private static readonly Menu Menu = new Menu("Auto Items", "Auto Items", true);
@@ -41,21 +42,27 @@ namespace AutoItems
             me = ObjectMgr.LocalHero;
             if (me == null)
                 return;
-
-            item_bottle = me.FindItem("item_bottle");
-            item_phase_boots = me.FindItem("item_phase_boots");
-            item_magic_stick = me.FindItem("item_magic_stick");
-            item_magic_wand = me.FindItem("item_magic_wand");
-            PercentStickUse =((double)Menu.Item("Percent Configuration").GetValue<Slider>().Value / 100);
-            if (me.IsAlive && (!me.IsInvisible() || me.ClassID == ClassID.CDOTA_Unit_Hero_Riki)  && !me.IsChanneling() && me.CanUseItems() && Utils.SleepCheck("AutoItems"))
+            if (me.IsAlive && (!me.IsInvisible() || me.ClassID == ClassID.CDOTA_Unit_Hero_Riki) && !me.IsChanneling() && Utils.SleepCheck("AutoItems"))
             {
-                if (item_bottle != null && me.Modifiers.Any(x => x.Name == "modifier_fountain_aura_buff" && (x.RemainingTime == 0 || x.RemainingTime >= 2)) && !me.Modifiers.Any(x => x.Name == "modifier_bottle_regeneration") && (me.Health < me.MaximumHealth || me.Mana < me.MaximumMana))
-                    item_bottle.UseAbility();
-                if (item_phase_boots != null && item_phase_boots.CanBeCasted() && me.NetworkActivity == NetworkActivity.Move)
+                item_bottle = me.FindItem("item_bottle");
+                item_phase_boots = me.FindItem("item_phase_boots");
+                item_magic_stick = me.FindItem("item_magic_stick");
+                item_magic_wand = me.FindItem("item_magic_wand");
+                PercentStickUse = ((double)Menu.Item("Percent Configuration").GetValue<Slider>().Value / 100);
+                if (item_bottle != null && me.Modifiers.Any(x => x.Name == "modifier_fountain_aura_buff") && _item_config.Item("Items: ").GetValue<AbilityToggler>().IsEnabled(item_bottle.Name))
+                {
+                    if(!me.Modifiers.Any(x => x.Name == "modifier_bottle_regeneration") && (me.Health < me.MaximumHealth || me.Mana < me.MaximumMana))
+                        item_bottle.UseAbility();
+                    Alies = ObjectMgr.GetEntities<Hero>().Where(x => x.Team == me.Team && x != me && (x.Health < x.MaximumHealth || x.Mana < x.MaximumMana) && !x.Modifiers.Any(y => y.Name == "modifier_bottle_regeneration") && x.IsAlive && !x.IsIllusion && x.Distance2D(me) <= item_bottle.CastRange).ToList();
+                    foreach (Hero v in Alies)
+                        if (v != null)
+                            item_bottle.UseAbility(v);
+                }
+                if (item_phase_boots != null && item_phase_boots.CanBeCasted() && me.NetworkActivity == NetworkActivity.Move && _item_config.Item("Items: ").GetValue<AbilityToggler>().IsEnabled(item_phase_boots.Name))
                     item_phase_boots.UseAbility();
-                if (item_magic_stick != null && item_magic_stick.CanBeCasted() && item_magic_stick.CurrentCharges > 0 && (double)me.Health / me.MaximumHealth < PercentStickUse)
+                if (item_magic_stick != null && item_magic_stick.CanBeCasted() && item_magic_stick.CurrentCharges > 0 && (double)me.Health / me.MaximumHealth < PercentStickUse && _item_config.Item("Items: ").GetValue<AbilityToggler>().IsEnabled(item_magic_stick.Name))
                     item_magic_stick.UseAbility();
-                if (item_magic_wand != null && item_magic_wand.CanBeCasted() && item_magic_wand.CurrentCharges > 0 && (double) me.Health / me.MaximumHealth < PercentStickUse)
+                if (item_magic_wand != null && item_magic_wand.CanBeCasted() && item_magic_wand.CurrentCharges > 0 && (double)me.Health / me.MaximumHealth < PercentStickUse && _item_config.Item("Items: ").GetValue<AbilityToggler>().IsEnabled(item_magic_wand.Name))
                     item_magic_wand.UseAbility();
                 Utils.Sleep(250, "AutoItems");
             }
